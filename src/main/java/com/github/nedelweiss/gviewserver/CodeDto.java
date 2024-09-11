2d0c308fde8ace444c03f9729e18a5cbbb5d38bf
nedelweiss <edelweissnull@gmail.com>
2024-09-12 00:30:57 +0300

    Removed @Getter annotation

--- src/main/java/com/github/nedelweiss/gviewserver/CodeDto.java
+++ src/main/java/com/github/nedelweiss/gviewserver/CodeDto.java
@@ -1,9 +1,7 @@
 package com.github.nedelweiss.gviewserver;
 
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-@Getter
 @RequiredArgsConstructor
 public record CodeDto(String code) {
 
